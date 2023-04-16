ad894835645f571c0760ad618d073a568234ff8a
ec9700 <112403058+ec9700@users.noreply.github.com>
2023-04-16 15:43:23 -0400

    RectGDX update and updated Javadocs
    Improved sorting
    Improved layering
    Add GUI
    Other minor improvements probably

--- Javadocs/package-search-index.js
+++ Javadocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.mygdx.game"},{"l":"com.mygdx.game.Components"},{"l":"com.mygdx.game.rectGDX"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.rectgdx"}]
